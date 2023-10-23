2764d794df437651df89a6438f1d1fc5399fdbce
Solivan Hiep <hiepsolivan@csu.fullerton.edu>
2023-10-23 15:32:18 -0700

    Finished TODO

--- part-1/datediff.cc
+++ part-1/datediff.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Solivan Hiep
+// HiepSolivan@csu.fullerton.edu
+// @HiepSolivan
+// Partners: @Kaleb16
 
 //
 // Program to calculate the date between two Gregorian dates.
@@ -13,46 +16,73 @@
 
 // Main function - the entry point to our program
 int main(int argc, char* argv[]) {
-  // TODO: Convert argv into a std::vector<std::string> using the
+  // DONE: Convert argv into a std::vector<std::string> using the
   // pattern described in lecture. Name the std::vector<std::string>
   // arguments.
+  std::vector<std::string> arguments{argv, argv + argc};
 
-  // TODO: Check to make sure there are enough arguments provided on the
+  // DONE: Check to make sure there are enough arguments provided on the
   // command line. If there are too few arguments, print the error message
   // "Please provide a starting date and an ending date. Exiting.\n"
   // and exit with a return code of 1.
+  if (arguments.size() != 7) {
+    std::cout
+        << "Please provide a starting date and an ending date. Exiting.\n";
+    return 1;
+  }
 
-  // TODO: Declare three integer variables and initialize them to zero.
+  // DONE: Declare three integer variables and initialize them to zero.
   // Name the variables start_month, start_day, start_year.
+  int start_month{0};
+  int start_day{0};
+  int start_year{0};
 
-  // TODO: For the std::string values at location 1, 2, and 3 in the
+  // DONE: For the std::string values at location 1, 2, and 3 in the
   // std::vector arguments, use std::stoi() to convert the string to
   // an integer.
   // Convert and assign the argument at location 1 to start_month.
   // Convert and assign the argument at location 2 to start_day.
   // Convert and assign the argument at location 3 to start_year.
+  start_month = std::stoi(arguments[1]);
+  start_day = std::stoi(arguments[2]);
+  start_year = std::stoi(arguments[3]);
 
-  // TODO: Declare three integer variables and initialize them to zero.
+  // DONE: Declare three integer variables and initialize them to zero.
   // Name the variables end_month, end_day, end_year.
+  int end_month{0};
+  int end_day{0};
+  int end_year{0};
 
-  // TODO: For the std::string values at location 4, 5, and 6 in the
+  // DONE: For the std::string values at location 4, 5, and 6 in the
   // std::vector arguments, use std::stoi() to convert the string to
   // an integer.
   // Convert and assign the argument at location 4 to end_month.
   // Convert and assign the argument at location 5 to end_day.
   // Convert and assign the argument at location 6 to end_year.
+  end_month = std::stoi(arguments[4]);
+  end_day = std::stoi(arguments[5]);
+  end_year = std::stoi(arguments[6]);
 
-  // TODO: Declare an integer variable namd number_days. Initialize the
+  // DONE: Declare an integer variable namd number_days. Initialize the
   // variable to the return value of DateDifference(). Call DateDifference()
-  // with the start_month, start_day, start_yuear, end_month, end_day, end_year.
+  // with the start_month, start_day, start_year, end_month, end_day, end_year.
+  int number_days{DateDifference(start_month, start_day, start_year, end_month,
+                                 end_day, end_year)};
 
-  // TODO: Using std::cout, print "The number of days between "
-  // TODO: Use the function PrintDate() to print the start date
-  // TODO: Using std::cout, print " and "
-  // TODO: Use the function PrintDate() to print the end date
-  // TODO: Using std::cout, print " is "
-  // TODO: Using std::cout, print the variable named number_days
-  // TODO: Using std::cout, print " days\n"
+  // DONE: Using std::cout, print "The number of days between "
+  std::cout << "The number of days between ";
+  // DONE: Use the function PrintDate() to print the start date
+  PrintDate(start_month, start_day, start_year);
+  // DONE: Using std::cout, print " and "
+  std::cout << " and ";
+  // DONE: Use the function PrintDate() to print the end date
+  PrintDate(end_month, end_day, end_year);
+  // DONE: Using std::cout, print " is "
+  std::cout << " is ";
+  // DONE: Using std::cout, print the variable named number_days
+  std::cout << number_days;
+  // DONE: Using std::cout, print " days\n"
+  std::cout << " days\n";
 
   return 0;
 }
